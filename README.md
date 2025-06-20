2f2e303223f65db0f284229453ffd4b4902be7fe
RiveraMg <135870777+RiveraMg@users.noreply.github.com>
2025-06-20 11:02:43 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+iMenssage sistema interactivo de mensaje es un programa hecho por mi que permite una interaccion con el usuario de recibir y mostrar mensajes de textos.
+
+Funcionamiento.
+1. realice la arquitectura de las carpetas, cree cada carpeta correspondiente a su archivo.
+EN total son 3 crapetas que cada una tiene:
+- SRC: tiene el archivo .js de sistema de mensajes.
+    1.USO: Puede ser copiado y ejecutado desde la consola de un navegador.
+
+    2.USO: en la carpeta raiz hay un index.html, en la carpeta asset esta el archivo style.css y en la carpeta img esta la imagen de fondp, sabiendo esto. En tu entorno de trabajo bien sea Visual Studio Code, puedes usar varias herramientas para visualizar el archivo html el cual tiene un boton que permite la interaccion e intercambio de mensajes. Extenciones como live server permite visualizar la esctructura y estilo del html, asi como tambien segun su preferencia puede hacerlo directamnete desde el navegador tambien.
+
+Para el uso de el lenguaje de JS es necesario que tenga instalada nodejs, recomendable en una version mayor a 18.0.
---
+++ src/sistema_interactivo.js
@@ -0,0 +1,35 @@
+function sistemaMensajes(){
+
+    //impremir un mensaje de bienvenida al usuario
+    console.log('Bienvenido a I-MENSSAGE Tu sistema interactivo de mensaje de texto!');
+
+    //recibir los datos que el usuario ingrese por medio de inputs
+    let nombre = prompt('Hola! como te llamas?')
+     //validaciones e imprimir mensajes dde errores
+    if (nombre === '' || nombre === null){
+        alert('ERROR: VALOR INVALIDO, por favor ingrese una nombre, este campo no debe estar vacio')
+        console.error('ERROR: VALOR INVALIDO, por favor ingrese una nombre, este campo no debe estar vacio');
+    } else {
+        let edad = prompt(`Hola ${nombre}! Cuantos años tienes?`)
+        //parseo de edad a INT
+        edad = parseInt(edad)
+    
+        //validaciones e imprimir mensajes dde errores
+        if (isNaN(edad)){
+            alert('ERROR: VALOR INVALIDO, por favor ingrese una edad valida!')
+            console.error('ERROR: VALOR INVALIDO, por favor ingrese una edad valida!');
+    
+        } else if (edad > 110 || edad <1){
+            alert('ERROR: VALOR INVALIDO, por favor ingrese una edad valida!')
+            console.error('ERROR: VALOR INVALIDO, por favor ingrese una edad valida!');
+    
+        } else if (edad < 18){
+            alert(`Bienvenido ${nombre}, Eres menor de edad. Puedes seguir aprendiendo y disfutando de lo que te apasiona!`);
+        
+        } else {
+            alert(`Hola ${nombre}, Eres mayor de edad. Estas list@ para disfrutar de las oportunidades y todos los retos que el mundo de la programacion tiene para ti?`)
+        }
+    }
+
+
+}
